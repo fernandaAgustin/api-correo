e1c4a4f2f58480f868fc136c189c99fe11d14d98
fernandaAgustin <al222310475@gmail.com>
2026-03-10 16:43:33 -0600

    Primer commit

---
+++ app.py
@@ -0,0 +1,40 @@
+
+from flask import Flask, request, jsonify
+import smtplib
+from email.mime.text import MIMEText
+import os
+
+app = Flask(__name__)
+
+SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
+SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")  # App Password si Gmail
+
+@app.route("/enviar", methods=["POST"])
+def enviar():
+
+    data = request.json
+    destinatario = data["email"]
+    asunto = data["subject"]
+    html = data["html"]
+
+    msg = MIMEText(html, "html")
+    msg["Subject"] = asunto
+    msg["From"] = SMTP_USER
+    msg["To"] = destinatario
+
+    try:
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(SMTP_USER, SMTP_PASSWORD)
+        server.send_message(msg)
+        server.quit()
+        return jsonify({"status": "ok"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+    
+# Configuración para Render
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
